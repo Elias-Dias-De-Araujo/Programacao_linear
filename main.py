ad89211a1257465ac63a352160d127761bb09e3d
rafaelsilva81 <rafaelgaldinosilva81@gmail.com>
2022-07-04 15:50:48 -0300

    Finalização do trabalho, resultado em CSV

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import csv
 import time
 import numpy as np
-from q1 import q1
-from q2 import q2
+from primal import q1
+from dual import q2
 
 
 def generateProblem():
@@ -19,44 +20,51 @@ def generateProblem():
 def main():
     i = 0
     print("GERANDO...")
-    while (i < 10):
-        data = generateProblem()
-        # Primal
-        primalTime = time.perf_counter()
-        x = q1(data[0], data[1], data[2], i, 'GLOP', 'q1')
-        x.resolveProblem()
-        finalPrimalTime = time.perf_counter()
+    header = ['Metodo', 'Solução Encontrada', 'Tempo de execução (ms)']
+    file_name = 'results/' + "resultado" + time.strftime("%d-%m-%Y-%H_%M_%S")  + '.csv'
+    with open(file_name, 'w', encoding='UTF8', newline='') as f:
+        while (i < 10):
+            data = generateProblem()
 
-        # Primal ( restrição de integralidade )
-        primalTimeRes = time.perf_counter()
-        x1 = q1(data[0], data[1], data[2], i, 'SCIP', 'q1Restricao')
-        x1.resolveProblem()
-        finalPrimalTimeRes = time.perf_counter()
+            # Primal
+            primalTime = time.perf_counter()
+            x = q1(data[0], data[1], data[2], i, 'GLOP')
+            r1 = x.resolveProblem()
+            finalPrimalTime = time.perf_counter()   
+            t1 = finalPrimalTime - primalTime
 
-        # Dual
-        dualTime = time.perf_counter()
-        y = q2(data[0], data[1], data[2], i, 'GLOP', 'q2')
-        y.resolveProblem()
-        finalDualTime = time.perf_counter()
+            # Primal ( restrição de integralidade )
+            primalTimeRes = time.perf_counter()
+            x1 = q1(data[0], data[1], data[2], i, 'SCIP')
+            r2 = x1.resolveProblem()
+            finalPrimalTimeRes = time.perf_counter()
+            t2 = finalPrimalTimeRes - primalTimeRes
 
-        # Dual ( restrição de integralidade )
-        dualTimeRes = time.perf_counter()
-        y1 = q2(data[0], data[1], data[2], i, 'SCIP', 'q2Restricao')
-        y1.resolveProblem()
-        finalDualTimeRes = time.perf_counter()
+            # Dual
+            dualTime = time.perf_counter()
+            y = q2(data[0], data[1], data[2], i, 'GLOP')
+            r3 = y.resolveProblem()
+            finalDualTime = time.perf_counter()
+            t3 = finalDualTime - dualTime
 
-        print("Instância " + str(i+1))
-        print(
-            f"Tempo de execução do primal: {finalPrimalTime - primalTime:0.4f} segundos")
-        print(
-            f"Tempo de execução do primal ( restrição de integralidadde ): {finalPrimalTimeRes - primalTimeRes:0.4f} segundos")
-        print(
-            f"Tempo de execução do dual: {finalDualTime - dualTime:0.4f} segundos")
-        print(
-            f"Tempo de execução do dual ( restrição de integralidadde ): {finalDualTimeRes - dualTimeRes:0.4f} segundos")
+            # Dual ( restrição de integralidade )
+            dualTimeRes = time.perf_counter()
+            y1 = q2(data[0], data[1], data[2], i, 'SCIP')
+            r4 = y1.resolveProblem()
+            finalDualTimeRes = time.perf_counter()
+            t4 = finalDualTimeRes - dualTimeRes
 
-        i += 1
-    print("CONCLUIDO!")
+            writer = csv.writer(f)
+            writer.writerow(['ITERAÇÃO: ', i])
+            writer.writerow(header)
+            writer.writerow(['Primal', r1, t1])
+            writer.writerow(['Primal (Restrição de Integralidade)', r2, t2])
+            writer.writerow(['Dual', r3, t3])
+            writer.writerow(['Dual (Restrição de Integralidade)', r4, t4])
+            writer.writerow('')
+            writer.writerow('')
+            i += 1
+        print("CONCLUIDO!")
 
 
 if __name__ == "__main__":
